489b8356b395d627339d89f70090412a3c9b0abd
malasen <71284217+malasen@users.noreply.github.com>
2023-03-02 16:11:31 +0100

    Chatroom realtime get functions

--- react-chat/src/components/Chatroom.tsx
+++ react-chat/src/components/Chatroom.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
+import { getFirestore, collection, limit, query, orderBy } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { FIREBASE_CONFIG } from "../constants/firebase";
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp(FIREBASE_CONFIG);
+const db = getFirestore(app);
+
+
 
 function Chatroom(props: {user: string}) {
+    type Message = {
+        user?: number;
+        text?: string;
+        createdAt?: Date;
+      };
+      
+    const msgRef = collection(db, "messages");
+    const msgQuery = query(msgRef, orderBy("createdAt", "asc"), limit(10));
+    const [messages, loading, error] = useCollectionData<Message>(msgQuery);
+ 
   return (
     <Container>
-        <ul id="messages"></ul>
+        <ul id="messages">
+        {messages?.map((value, index, array) => {
+            return(
+                <div key={index}>
+                    <p >{value.user}: {value.text}</p>
+                </div>)
+            })}
+        </ul>
         <form id="form" action="">
             <input id="input" autoComplete="off" />
             <Button variant="secondary">Send</Button>
