8fad8df000f774276e2c11837a4df831f6a9a241
malasen <71284217+malasen@users.noreply.github.com>
2023-03-03 00:52:44 +0100

    beta branch merge deploy test

--- react-chat/src/App.tsx
+++ react-chat/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
                   <Welcome />
                   <div className="mt-5">
                     <Button href="#" variant="primary" size="lg" onClick={logIn}>
-                      Fortsett til Chatten 
+                      Fortsett til chatten 
                       <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16"> 
                          <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                       </svg>
@@ -33,7 +33,7 @@ function App() {
             ) : (
                 <>
                   <div className="Chat-header d-flex justify-content-between">
-                    <h1>Chatroom</h1>
+                    <h1>Chat</h1>
                     <Button variant="dark" onClick={logOut}>Logout</Button>
                   </div>
                   <Chatroom user={user}/>
